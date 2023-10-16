1e845a6039e8b8509f890a236a3677f97b79c347
Czirják Zoltán <czirjak.zoltan91@gmail.com>
2023-10-16 17:27:04 +0200

    adding ascp

--- infrastructure/03-infrastructure/eks-11-csi-driver.tf
+++ infrastructure/03-infrastructure/eks-11-csi-driver.tf
@@ -1,24 +1,24 @@
 resource "helm_release" "secrets-store-csi-driver" {
-  depends_on       = [
+  depends_on = [
     aws_eks_cluster.eks-cluster,
     aws_eks_node_group.node-ec2
   ]
 
   count = var.kubernetes-enabled && var.database-enabled ? 1 : 0
 
-  name             = "secrets-store-csi-driver"
-  repository       = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts/"
-  chart            = "secrets-store-csi-driver"
-  namespace        = "kube-system"
+  name       = "secrets-store-csi-driver"
+  repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts/"
+  chart      = "secrets-store-csi-driver"
+  namespace  = "kube-system"
 }
 
 resource "helm_release" "ascp" {
-  depends_on       = [ helm_release.secrets-store-csi-driver ]
+  depends_on = [helm_release.secrets-store-csi-driver]
 
   count = var.kubernetes-enabled && var.database-enabled ? 1 : 0
 
-  name             = "csi-secrets-store-provider-aws"
-  repository       = "https://aws.github.io/secrets-store-csi-driver-provider-aws/charts/"
-  chart            = "csi-secrets-store-provider-aws"
-  namespace        = "kube-system"
+  name       = "secrets-provider-aws"
+  namespace  = "kube-system"
+  repository = "https://aws.github.io/secrets-store-csi-driver-provider-aws"
+  chart      = "secrets-store-csi-driver-provider-aws"
 }
